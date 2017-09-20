4c98bfd5b89c28d3ca8734bae37cae6b48b82fb2
Tomáš Hotový <th.thotovy@gmail.com>
2017-09-20 17:45:28 +0200

    Create Sound.java

---
+++ Rain/Sound.java
@@ -0,0 +1,33 @@
+import java.io.File;
+
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
+
+public class Sound {
+	private Clip clip;
+	private FloatControl volume;
+	public Sound() {
+		try {
+			clip = AudioSystem.getClip();
+			clip.open(AudioSystem.getAudioInputStream(new File("rain_low.wav")));
+			volume = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+			volume.setValue(-6);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public void stop() {
+		clip.stop();
+	}
+	public void start() {
+		clip.loop(Clip.LOOP_CONTINUOUSLY);
+	}
+	public void setVolume(int vol) {
+		clip.flush(); //causes short stutter but grants real-time volume adjusting 
+		volume.setValue(-6+vol/4);
+	}
+	public boolean isRunning() {
+		return clip.isRunning();
+	}
+}
